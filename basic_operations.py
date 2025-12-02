1f8caf216e8e4fb49ae95c61f09d51d9b17385b3
DrCode <bumaadumbari@gmail.com>
2025-12-02 21:39:35 +0100

    update

--- basic_operations.py
+++ basic_operations.py
@@ -1,9 +1,9 @@
 number1 = 10
 number2 = 5
 sum = number1 + number2
-print("The sum of", number1, "and", number2, "is:", sum)
+print("Addition of", number1, "and", number2, "is", sum)
 difference = number1 - number2  
-print("The difference between", number1, "and", number2, "is:", difference)
+print("Subtraction of", number1, "and", number2, "is", difference)
 product = number1 * number2  
-print("The product of", number1, "and", number2, "is:", product
+print("Multiplication of", number1, "and", number2, "is", product
 )
